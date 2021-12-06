f176bd2e604ba7cd0bb103467cf29ac54577800e
@khanzhin_aleksandr <bebezaz@yandex.ru>
2021-12-06 21:28:36 +0300

    added search function

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -121,12 +122,12 @@ public class DataBase {
         pageIdResultSet.close();
         preparedStatement.close();
 
+
         connection.commit();
         return pageId;
     }
 
     public static Map<String, Integer> insertLemmsAndGetId(Set<String> lemmaSet) throws SQLException {
-        Map<String, Integer> lemmaMap = new HashMap<>();
         String insertQuery = "INSERT INTO _lemma"
                 + " (lemma, frequency) VALUES ( ?, ?) ON DUPLICATE KEY UPDATE frequency = frequency + 1;";
 
@@ -140,6 +141,7 @@ public class DataBase {
         connection.commit();
 
         ResultSet insertedIds = insertLemmaData.getGeneratedKeys();
+        Map<String, Integer> lemmaMap = new HashMap<>();
         for (String lemma : lemmaSet) {
             insertedIds.next();
             lemmaMap.put(lemma, insertedIds.getInt(1));
@@ -178,4 +180,152 @@ public class DataBase {
         statement.setFloat(4, rank);
         statement.addBatch();
     }
+
+    public static ResultSet getLemmas(Set<String> lemmas) throws SQLException {
+
+        String query = createMultipleLemmaQuery(lemmas.size());
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        int parameterIndex = 1;
+        for (String lemma : lemmas) {
+            preparedStatement.setString(parameterIndex++, lemma);
+        }
+
+        return preparedStatement.executeQuery();
+    }
+
+    private static String createMultipleLemmaQuery(int length) {
+        String query = "SELECT id FROM _lemma WHERE lemma IN (";
+        StringBuilder queryBuilder = new StringBuilder(query);
+        appendValueForQuery(queryBuilder, length);
+        queryBuilder.append(" ) ORDER BY frequency;");
+        return queryBuilder.toString();
+    }
+
+
+    public static ArrayList<Integer> getPagesOfFirstLemma(int lemmaId) throws SQLException {
+        String query = "SELECT id FROM _page WHERE id in (SELECT page_id FROM _index where lemma_id = " + lemmaId + ")";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ResultSet pagesResultSet = preparedStatement.executeQuery();
+        ArrayList<Integer> pagesIdList = resultSetToList(pagesResultSet);
+        preparedStatement.close();
+
+        return pagesIdList;
+    }
+
+    public static ArrayList<Integer> getSearchedPages(int lemmaId, ArrayList<Integer> pagesIdList) throws SQLException {
+
+        String query = createMultiplePageFromLemmaQuery(pagesIdList.size());
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, lemmaId);
+
+        int parameterIndex = 2;
+        for (int pageId : pagesIdList) {
+            preparedStatement.setInt(parameterIndex++, pageId);
+        }
+        ResultSet pagesResultSet = preparedStatement.executeQuery();
+
+        ArrayList<Integer> filteredPagesIdList = resultSetToList(pagesResultSet);
+        preparedStatement.close();
+
+        return filteredPagesIdList;
+    }
+
+    private static String createMultiplePageFromLemmaQuery(int length) {
+        String query = "SELECT id FROM _page WHERE id IN (SELECT page_id FROM _index WHERE lemma_id = ? " +
+                "AND page_id in (";
+        StringBuilder queryBuilder = new StringBuilder(query);
+        appendValueForQuery(queryBuilder, length);
+        queryBuilder.append(" ))");
+        return queryBuilder.toString();
+    }
+
+    public static ArrayList<PageSearchData> getPageRelevanceAndPathAndHtml(ArrayList<Integer> pageIdList,
+                                                              ArrayList<Integer> lemmaIdList) throws SQLException {
+        String firstPartOFQueryRel = "SELECT page_id, sum(lemma_rank)/relrev.maxrev as relevance from _index join " +
+                "(select max(absrev) as maxrev from (select page_id, sum(lemma_rank) as absrev from _index ";
+        String wherePartOfQueryRel = createMultipleRelevanceQuery(pageIdList.size(), lemmaIdList.size());
+        StringBuilder queryBuilder = new StringBuilder(firstPartOFQueryRel);
+        queryBuilder.append(wherePartOfQueryRel);
+        queryBuilder.append(" group by page_id) as result) as relrev ");
+        queryBuilder.append(wherePartOfQueryRel);
+        queryBuilder.append(" group by page_id order by page_id");
+
+        PreparedStatement relevancePrepStatement = connection.prepareStatement(queryBuilder.toString());
+        int parameterIndexForRel = 1;
+        for (int pageId : pageIdList) {
+            relevancePrepStatement.setInt(parameterIndexForRel++, pageId);
+        }
+        for (int pageId : lemmaIdList) {
+            relevancePrepStatement.setInt(parameterIndexForRel++, pageId);
+        }
+        for (int pageId : pageIdList) {
+            relevancePrepStatement.setInt(parameterIndexForRel++, pageId);
+        }
+        for (int pageId : lemmaIdList) {
+            relevancePrepStatement.setInt(parameterIndexForRel++, pageId);
+        }
+
+        String getPageData = createMultiplePageQuery(pageIdList.size());
+        PreparedStatement pageDataPrepStatement = connection.prepareStatement(getPageData);
+        int parameterIndexForPageData = 1;
+        for (int pageId : pageIdList) {
+            pageDataPrepStatement.setInt(parameterIndexForPageData++, pageId);
+        }
+
+        ResultSet pagesRelResultSet = relevancePrepStatement.executeQuery();
+        ResultSet pageDataResultSet = pageDataPrepStatement.executeQuery();
+
+        ArrayList<PageSearchData> pageSearchDataList = new ArrayList<>();
+        while (pagesRelResultSet.next()){
+            pageDataResultSet.next();
+            PageSearchData page = new PageSearchData(pagesRelResultSet.getInt(1),
+                    pageDataResultSet.getString(2), pageDataResultSet.getString(3),
+                    pagesRelResultSet.getFloat(2));
+            pageSearchDataList.add(page);
+        }
+
+        pagesRelResultSet.close();
+        relevancePrepStatement.close();
+        pageDataResultSet.close();
+        pageDataPrepStatement.close();
+        return pageSearchDataList;
+    }
+
+    private static String createMultipleRelevanceQuery(int pageIdLength, int lemmaIdLength) {
+        String query = "WHERE page_id IN (";
+        StringBuilder queryBuilder = new StringBuilder(query);
+        appendValueForQuery(queryBuilder, pageIdLength);
+        queryBuilder.append(" ) AND lemma_id IN (");
+        appendValueForQuery(queryBuilder, lemmaIdLength);
+        queryBuilder.append(")");
+
+        return queryBuilder.toString();
+    }
+
+    private static String createMultiplePageQuery(int length) {
+        String query = "SELECT id, `path`, content from _page WHERE id IN(";
+        StringBuilder queryBuilder = new StringBuilder(query);
+        appendValueForQuery(queryBuilder, length);
+        queryBuilder.append(" ) ORDER BY id");
+        return queryBuilder.toString();
+    }
+
+    private static void appendValueForQuery(StringBuilder queryBuilder, int length) {
+        for (int i = 0; i < length; i++) {
+            queryBuilder.append(" ?");
+            if (i != length - 1)
+                queryBuilder.append(",");
+        }
+    }
+
+    public static ArrayList<Integer> resultSetToList(ResultSet resultSet) throws SQLException {
+        ArrayList<Integer> list = new ArrayList<>();
+        while (resultSet.next()) {
+            list.add(resultSet.getInt(1));
+        }
+        resultSet.close();
+        return list;
+    }
+
 }
--- src/main/java/JsoupData.java
+++ src/main/java/JsoupData.java
@@ -1,5 +1,6 @@
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -20,11 +21,47 @@ public class JsoupData {
         return connection.get().getElementsByTag("a");
     }
 
-    public static String getTitleText(Connection connection) throws InterruptedException, IOException {
+    public static String getTitleText(Connection connection) throws IOException {
         return connection.get().title();
     }
 
-    public static String getBodyText(Connection connection) throws InterruptedException, IOException {
+    public static String getBodyText(Connection connection) throws IOException {
         return connection.get().body().text();
+    }
+
+    public static String getSearchedTextInHtml(String htmlText, String searchQuery) {
+        Document doc = Jsoup.parse(htmlText);
+        String textOfSearchQuery = doc.getElementsContainingOwnText(searchQuery).text();
+
+        if (!textOfSearchQuery.isEmpty()) {
+            int firstIndexOfSnippet = textOfSearchQuery.indexOf(searchQuery) > 20 ?
+                    textOfSearchQuery.indexOf(searchQuery) - 20 : 0;
+            int lastIndexOfSnippet = firstIndexOfSnippet + searchQuery.length() + 30 < textOfSearchQuery.length() - 1 ?
+                    firstIndexOfSnippet + searchQuery.length() + 30 : textOfSearchQuery.length() - 1;
+
+            return "<p>" + textOfSearchQuery.substring(firstIndexOfSnippet, lastIndexOfSnippet)
+                    .replace(searchQuery, "<b>" + searchQuery + "</b>") + "</p>";
+        }
+
+        StringBuilder snippetBuilder = new StringBuilder();
+        String[] queryWords = searchQuery.split(" ");
+
+        for (String word : queryWords) {
+            String substring = word.substring(0, word.length() - 2);
+            String textOfSearchWord = doc.getElementsContainingOwnText(substring).text();
+            int firstIndexOfSnippet = textOfSearchWord.indexOf(word) > 20 ?
+                    textOfSearchWord.indexOf(word) - 20 : 0;
+            int lastIndexOfSnippet = firstIndexOfSnippet + word.length() + 37 < textOfSearchWord.length() - 1 ?
+                    firstIndexOfSnippet + word.length() + 37 : textOfSearchWord.length() - 1;
+            String snippetPart = "<p>" + textOfSearchWord.substring(firstIndexOfSnippet, lastIndexOfSnippet)
+                    .replaceAll("(?i)\\w?" + substring, "<b>" + substring + "</b>") + "</p>";
+            snippetBuilder.append(snippetPart);
+        }
+
+        return snippetBuilder.toString();
+    }
+
+    public static String getTitle(String htmlText){
+        return Jsoup.parse(htmlText).title();
     }
 }
